b2dfde12367a35a63ac96fa21d336f86263acc31
Yuriy <xstream_06@mail.ru>
2022-12-03 08:28:36 +0300

    Lesson1_HWUpdate

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-temporary layout
+let arr = [], set = new Set(), n = 1000000;
+for (let i = 0; i < n; i++) {
+  arr.push(i);
+  set.add(i);
+};
+
+let result;
+
+console.time('Array');
+result = arr.indexOf(123123) !== -1;
+console.timeEnd('Array');
+
+console.time('Set');
+result = set.has(123123);
+console.timeEnd('Set');
+
+console.time('Array'); 
+arr.push(n);
+console.timeEnd('Array');
+console.time('Set'); 
+set.add(n);
+console.timeEnd('Set');
+
+const deleteFromArr = (arr, item) => {
+    let index = arr.indexOf(item);
+    return index !== -1 && arr.splice(index, 1);
+  };
+  console.time('Array'); 
+deleteFromArr(arr, n);
+console.timeEnd('Array');
+console.time('Set'); 
+set.delete(n);
+console.timeEnd('Set');
